c684d39c1108733b5293e14fa9d07261e757d558
dogaatac <dogaatac1905@gmail.com>
2023-09-01 00:51:14 +0300

    Update blade.sh

--- blade.sh
+++ blade.sh
@@ -3,7 +3,7 @@ echo "Basladi"
 while [ true ]
 do
 cd /root/
-./bladebit_cuda -c xch1k96y24rsj5eqdzn6kfhqjxs3w9886fpat8ryj6r0j3q56grshnwqtcxnuk -f aaf2e7d71a05b5eb44aac91401987d293c218b8a4c37e451b2895ffaec4967cba911ac2af29c732fdbdddd2b2e78635b --compress 1 -n 111 cudaplot --disk-128 -t1 /mnt/temp /mnt/pw/
+./bladebit_cuda -c xch1k96y24rsj5eqdzn6kfhqjxs3w9886fpat8ryj6r0j3q56grshnwqtcxnuk -f aaf2e7d71a05b5eb44aac91401987d293c218b8a4c37e451b2895ffaec4967cba911ac2af29c732fdbdddd2b2e78635b --compress 4 -n 111 cudaplot --disk-128 -t1 /mnt/temp /mnt/pw/
 sleep 10
 cd /mnt/temp/
 rm *
