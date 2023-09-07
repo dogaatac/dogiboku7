6676d253d85bc6f0c1f81718c89081aabab729b4
dogaatac <dogaatac1905@gmail.com>
2023-09-07 08:53:47 +0300

    Update up1.sh

--- up1.sh
+++ up1.sh
@@ -9,7 +9,7 @@ while true
 do
     index=$((($RANDOM % $file_count) + 1))
                
-        rclone move /mnt/up1/ "up$slot"crypt: --include "*.plot" --buffer-size=32M --drive-chunk-size=16M --drive-upload-cutoff=1000T --drive-pacer-min-sleep=700ms --checksum --check-first --drive-acknowledge-abuse --copy-links --drive-stop-on-upload-limit --no-traverse --tpslimit-burst=0 --retries=3 --low-level-retries=10 --checkers=14 --tpslimit=3 --transfers=3 --fast-list --user-agent="ISV|rclone.org|rclone/v1.61.0-beta.6610.beea4d511"  --drive-service-account-file "/root/.config/rclone/accounts/$index.json" -P
+        rclone move /mnt/up1/ "up$slot"crypt: --include "*.plot" --buffer-size=128M --drive-chunk-size=128M --drive-upload-cutoff=1000T --drive-pacer-min-sleep=700ms --checksum --check-first --drive-acknowledge-abuse --copy-links --drive-stop-on-upload-limit --no-traverse --tpslimit-burst=0 --retries=3 --low-level-retries=10 --checkers=14 --tpslimit=3 --transfers=3 --fast-list --user-agent="ISV|rclone.org|rclone/v1.61.0-beta.6610.beea4d511"  --drive-service-account-file "/root/.config/rclone/accounts/$index.json" -P
         if [[ $? -eq 0 ]]; then
             echo -e "\e[3;32m Transfer Done ... \e[0m"
         else
