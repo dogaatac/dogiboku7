b4895c6a5562a4fd03e60395648bb92fd799d99c
dogaatac <dogaatac1905@gmail.com>
2023-09-22 09:53:37 +0300

    Create found.sh

---
+++ found.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Basladi"
+
+while true
+do
+    cd /.chia/mainnet/log/
+    rm *
+    cd
+    sleep 5
+    screen -S found -X quit
+    screen -dmS found python3 found.py
+
+    sleep 15000
+done
